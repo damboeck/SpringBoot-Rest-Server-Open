effb903493937025c885c12b8e0ddb2fc094b5c1
Werner <werner.damboeck@letto.at>
2023-02-27 08:38:20 +0100

    Initial commit

---
+++ src/main/java/at/ac/htlstp/demo/springbootrestserveropen/SpringBootRestServerOpenApplication.java
@@ -0,0 +1,13 @@
+package at.ac.htlstp.demo.springbootrestserveropen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestServerOpenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestServerOpenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/ac/htlstp/demo/springbootrestserveropen/SpringBootRestServerOpenApplicationTests.java
@@ -0,0 +1,13 @@
+package at.ac.htlstp.demo.springbootrestserveropen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestServerOpenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
